9b04f2af9ebc0ea28f6e2f86033da657ba58bed0
joseph-x-li <13826851+joseph-x-li@users.noreply.github.com>
2021-05-18 03:01:02 -0700

    video audio test

--- qast.py
+++ qast.py
@@ -1,19 +1,33 @@
 import pystreaming, cv2, zmq, time
 import sounddevice as sd
 
+# set up camera
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+# set up microphone
+indevice = 0
+device_info = sd.query_devices(indevice, 'input')
+samplerate = int(device_info['default_samplerate'])
+import queue
+IN = queue.Queue()
+
+# set up stream backend
 stream = pystreaming.Streamer(zmq.Context(), "tcp://*:5555") 
 audiostream = pystreaming.AudioStreamer(zmq.Context(), "tcp://*:5556")
 stream.start()
 time.sleep(1)
 
+def callback_in(indata, frames, time, status):
+    audiostream.send(indata.copy())
+
 start, n = time.time(), 0
-while True:
-    n += 1
-    ret, frame = cap.read()
-    stream.send(frame)
-    print(f"\rFPS: {(n / (time.time() - start)):.3f}", end="")
+with sd.InputStream(samplerate=samplerate, device=indevice, channels=1, callback=callback_in):
+    while True:
+        n += 1
+        ret, frame = cap.read()
+        stream.send(frame)
+        print(f"\rFPS: {(n / (time.time() - start)):.3f}", end="")
+
